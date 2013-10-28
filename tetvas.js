0a094d59f64609a62679bef80ad271330a91a838
WalrusCow <willgmcd@gmail.com>
2013-10-27 19:22:31 -0500

    Dropping now works; arrow keys

--- tetvas.js
+++ tetvas.js
@@ -3,6 +3,38 @@ var Tetvas = (function() {
    * Grid should be 20 high and 10 across
    */
 
+  // Key constants
+  var UP_ARROW = 38;
+  var DOWN_ARROW = 40;
+  var LEFT_ARROW = 37;
+  var RIGHT_ARROW = 39;
+
+  var SPACE_BAR = 32;
+  var CTRL_KEY = 17;
+
+  var P_KEY = 80;
+  var X_KEY = 88;
+  var Z_KEY = 90;
+
+  // Constants for Blocks
+  var GRID_SIZE = 15;
+  var BORDER_COLOUR = '#000000';
+  var BORDER_WIDTH = 1;
+
+  // Initial speed of the game ticker
+  var START_SPEED = 1000;
+
+  // Colours for shapes
+  var SHAPE_FILLS = {
+    'I' : '#00FFFF',
+    'O' : '#FFFF00',
+    'T' : '#FF00FF',
+    'J' : '#0000FF',
+    'L' : '#FFA500',
+    'S' : '#00FF00',
+    'Z' : '#FF0000'
+  };
+
   // Canvas and context to use for everything
   // TODO : Consider a background and a foreground canvas?
   var canvas = document.getElementById('tetvas');
@@ -72,19 +104,14 @@ var Tetvas = (function() {
      * Block object to represent a square on the grid
      */
 
-    // Constants for Blocks
-    var SIZE = 15;
-    var BORDER_COLOUR = '#000000';
-    var BORDER_WIDTH = 1;
-
     // Return object
     var Block = {};
 
     Block.setPoint = function(pt) {
       /* Set new point for the block */
       this.gridPoint = copyPoint(pt);
-      this._x = SIZE * this.gridPoint.x;
-      this._y = SIZE * this.gridPoint.y;
+      this._x = GRID_SIZE * this.gridPoint.x;
+      this._y = GRID_SIZE * this.gridPoint.y;
     };
 
     // Set the point for the block
@@ -96,7 +123,7 @@ var Tetvas = (function() {
 
     Block.undraw = function() {
       /* Clear the block from the canvas */
-      ctx.clearRect(this._x, this._y, SIZE, SIZE);
+      ctx.clearRect(this._x, this._y, GRID_SIZE, GRID_SIZE);
     };
 
     Block.draw = function() {
@@ -108,11 +135,11 @@ var Tetvas = (function() {
 
       // Draw border square
       ctx.fillStyle = BORDER_COLOUR;
-      ctx.fillRect(this._x, this._y, SIZE, SIZE);
+      ctx.fillRect(this._x, this._y, GRID_SIZE, GRID_SIZE);
 
       // Offset for the border
       var fillPt = { x : this._x + BORDER_WIDTH, y : this._y + BORDER_WIDTH };
-      var fillSize = SIZE - (2 * BORDER_WIDTH);
+      var fillSize = GRID_SIZE - (2 * BORDER_WIDTH);
 
       // Draw fill square
       ctx.fillStyle = this.fill;
@@ -158,16 +185,6 @@ var Tetvas = (function() {
       'L' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 1, y : -1 }, { x : 1, y : 0 } ],
       'S' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : -1 } ],
       'Z' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : 0, y : -1 }, { x : -1, y : -1 } ]
-    };
-
-    var SHAPE_FILLS = {
-      'I' : '#00FFFF',
-      'O' : '#FFFF00',
-      'T' : '#FF00FF',
-      'J' : '#0000FF',
-      'L' : '#FFA500',
-      'S' : '#00FF00',
-      'Z' : '#FF0000'
     };
 
     // Return object
@@ -222,30 +239,6 @@ var Tetvas = (function() {
       }
     };
 
-    Piece.moveDown = function(frozenBlocks) {
-      /*
-       * Move the piece down one block
-       * Returns true if the move is successful (no intersection with frozenBlocks)
-       * Returns false if the move is unsuccessful
-       */
-
-      // Move origin; set new points for blocks
-      this.origin.y += 1;
-      this.undraw();
-      this.updateBlocks();
-
-      // Check if the new position intersects with any of the frozen blocks
-      if (this.intersects(frozenBlocks)) {
-        // It does intersect. Undo the move
-        this.origin.y -= 1;
-        this.updateBlocks();
-        this.draw();
-        return false;
-      }
-
-      this.draw();
-      return true;
-    };
 
     Piece.intersects = function(frozenBlocks) {
       /*
@@ -258,8 +251,73 @@ var Tetvas = (function() {
           return true;
         }
       }
-      return false;
 
+      return false;
+    };
+
+    Piece._move = function(frozenBlocks, axis, mag) {
+      /*
+       * Move the piece along the specified axis mag spaces
+       * Returns true if the move is successful (no intersection with frozenBlocks)
+       * Returns false if the move is unsuccessful
+       */
+
+      // Move along the axis
+      this.origin[axis] += mag;
+
+      // Undraw the blocks, since we might move (but we might not)
+      this.undraw();
+      // Update the blocks to their new positions
+      this.updateBlocks();
+
+      // Check for intersection
+      if (this.intersects(frozenBlocks)) {
+        // It does intersect. Undo the move
+        this.origin[axis] -= mag;
+        this.updateBlocks();
+        this.draw();
+        return false;
+      }
+
+      // No intersection
+      this.draw();
+      return true;
+    };
+
+    /* Functions to move various directions */
+    Piece.moveLeft = function(frozenBlocks) {
+      return this._move(frozenBlocks, 'x', -1);
+    };
+    Piece.moveRight = function(frozenBlocks) {
+      return this._move(frozenBlocks, 'x', 1);
+    };
+    Piece.moveDown = function(frozenBlocks) {
+      /* If the move failed then the piece freezes. */
+      var ret = this._move(frozenBlocks, 'y', 1);
+      return ret || this.freeze(frozenBlocks);
+    };
+
+    Piece.drop = function(frozenBlocks) {
+      /* Drop the piece. */
+      while(this.moveDown(frozenBlocks));
+    };
+
+    Piece.freeze = function(frozenBlocks) {
+      /* Freeze the piece. */
+      for (var i = 0; i < this.blocks.length; ++i) {
+        var coords = this.blocks[i].gridPoint;
+        frozenBlocks[coords.x][coords.y] = this.blocks[i];
+      }
+    };
+
+    Piece._rotate = function(dir) {
+      /* Rotate the piece in the specified direction. */
+    };
+    Piece.rotateRight = function() {
+      this._rotate(1);
+    };
+    Piece.rotateLeft = function() {
+      this._rotate(-1);
     };
 
     return Piece;
@@ -269,13 +327,14 @@ var Tetvas = (function() {
    * Main driver area
    *******************************************************/
 
-  // Initial speed of the game ticker
-  var START_SPEED = 1000;
-
   // Return object (game driver)
   var Tetvas = {};
 
+  // Initial speed
   Tetvas.speed = START_SPEED;
+
+  // Boolean to indicate if the game is ongoing
+  Tetvas.playing = false;
 
   // Initial index
   Tetvas.pieceIndex = 0;
@@ -284,21 +343,19 @@ var Tetvas = (function() {
   Tetvas.pieceGen = [ 'I', 'O', 'T', 'J', 'L', 'S', 'Z'];
 
   // Blocks that have been frozen, organized by columns and rows
-  // Initialize to empty objects
-
-  // Actually this should be initialized to a border of the game !!!
+  // We also have rows for the border, to make stopping the pieces
+  // at the border automatic
   Tetvas.frozenBlocks = {};
   for (var i = -1; i < 11; ++i) {
     Tetvas.frozenBlocks[i] = {};
   }
 
-  // Build the left and right columns
-  for (var i = 0; i < 20; ++i) {
+  // Build the left and right columns (to stop blocks)
+  for (var i = -1; i < 20; ++i) {
     Tetvas.frozenBlocks[-1][i] = true;
     Tetvas.frozenBlocks[10][i] = true;
   }
-
-  // Build the bottom row
+  // Build the bottom row (to stop blocks)
   for (var i = 0; i < 10; ++i) {
     Tetvas.frozenBlocks[i][20] = true;
   }
@@ -306,53 +363,111 @@ var Tetvas = (function() {
   Tetvas.getNextPiece = function() {
     /* Get the next piece to generate */
     if (!this.pieceIndex) { shuffle(this.pieceGen); }
-    return this.pieceGen[this.pieceIndex++ % this.pieceGen.length];
+
+    var ret = this.pieceGen[this.pieceIndex];
+
+    // Increment index;
+    this.pieceIndex += 1;
+    this.pieceIndex %= this.pieceGen.length;
+
+    return ret;
   };
 
   Tetvas.tick = function() {
     /* Function to represent one game tick */
 
     // Move the piece down (or try to)
-    if(!this.currentPiece.moveDown(this.frozenBlocks)) {
+    if(!this.piece.moveDown(this.frozenBlocks)) {
 
-      // Move failed.  Freeze the piece and generate a new one
-      var blocks = this.currentPiece.blocks;
-      for (var i = 0; i < blocks.length; ++i) {
-        var coords = blocks[i].gridPoint;
-        this.frozenBlocks[coords.x][coords.y] = blocks[i];
-      }
+      this.piece.freeze(this.frozenBlocks);
+
 
       // Generate a new piece
-      this.currentPiece = new Piece(this.getNextPiece());
+      this.piece = new Piece(this.getNextPiece());
     }
+  };
+
+  Tetvas.keyStroke = function(key) {
+    /* Handle a keystroke */
+
+    switch(key) {
+      case LEFT_ARROW:
+        this.piece.moveLeft(this.frozenBlocks);
+        break;
+
+      case RIGHT_ARROW:
+        this.piece.moveRight(this.frozenBlocks);
+        break;
+
+      case DOWN_ARROW:
+        this.piece.moveDown(this.frozenBlocks);
+        break;
+
+      case UP_ARROW:
+      case X_KEY:
+        this.piece.rotateRight(this.frozenBlocks);
+        break;
+
+      case Z_KEY:
+        this.piece.rotateLeft(this.frozenBlocks);
+        break;
+
+      case CTRL_KEY:
+      case SPACE_BAR:
+        this.piece.drop(this.frozenBlocks);
+        break;
+
+      case P_KEY:
+        this.togglePause();
+        break;
+    }
+  };
+
+  Tetvas.registerListeners = function() {
+    /* Register the keyboard listener for the game. */
+
+    var self = this;
+    // We listen to keydown event
+    document.addEventListener('keydown', function() {
+      self.keyStroke(event.keyCode);
+    }, true);
+
   };
 
   Tetvas.start = function() {
     /* Begin the game */
-    if (this.gameTicker) return;
+
+    // Don't start the game if it has already started
+    if (this.playing) return;
+
     // Start the game
     var self = this;
+    this.playing = true;
+
+    this.registerListeners();
+
     // Create the first piece
-    this.currentPiece = new Piece(this.getNextPiece());
+    this.piece = new Piece(this.getNextPiece());
+
     // Start the ticker
-    this.gameTicker = window.setInterval(function() {
-      self.tick();
-    }, this.speed);
+    this.togglePause();
   };
 
-  Tetvas.pause = function() {
-    /* Pause the game */
-    window.clearInterval(this.gameTicker);
-    this.gameTicker = null;
-  };
+  Tetvas.togglePause = function() {
+    /* Toggle game pause */
+    var self = this;
 
-  Tetvas.resume = function() {
-    /* Resume the game */
-    if (this.gameTicker) return;
-    // Start the ticker
-    this.gameTicker = window.setInterval(function() {
-      self.tick();
-    }, this.speed);
+    if (this.gameTicker) {
+      // It is playing - pause it
+      window.clearInterval(this.gameTicker);
+      this.gameTicker = null;
+
+    } else {
+      // Start the ticker
+      this.gameTicker = window.setInterval(function() {
+        self.tick();
+      }, this.speed);
+    }
   };
 
   // Testing ...
