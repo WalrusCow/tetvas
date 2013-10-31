94b29f14a8ff60ad1bd77c94372733de22a26de6
WalrusCow <willgmcd@gmail.com>
2013-10-30 22:55:35 -0500

    Ghost inherits from Piece

--- tetvas.js
+++ tetvas.js
@@ -1,16 +1,3 @@
-  function addPoints() {
-    /* Sum a list of points up */
-    var newPoint = { x : 0, y : 0 };
-    var lop;
-    var lop = arguments[0] instanceof Array ? arguments[0] : arguments;
-
-    for (var i = 0; i < lop.length; ++i) {
-      newPoint.x += lop[i].x;
-      newPoint.y += lop[i].y;
-    }
-    return newPoint;
-  }
-
 /*
  * Grid should be 20 high and 10 across
  */
@@ -51,6 +38,18 @@ var Tetvas = (function() {
     'Z' : '#ff0000'
   };
 
+  // Initial points (relative to piece origin) for each shape
+  // These completely define the shapes
+  var SHAPE_POINTS = {
+    'I' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : -1, y : 0 }, { x : 2, y : 0 } ],
+    'O' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : -1 } ],
+    'T' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : 0 } ],
+    'J' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : -1, y : -1 }, { x : 1, y : 0 } ],
+    'L' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 1, y : -1 }, { x : 1, y : 0 } ],
+    'S' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : -1 } ],
+    'Z' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : 0, y : -1 }, { x : -1, y : -1 } ]
+  };
+
   // Fill for ghost
   var GHOST_FILL = '#c3c3c3';
 
@@ -62,6 +61,8 @@ var Tetvas = (function() {
   /*******************************************************
    * Utility functions
    *******************************************************/
+
+  function nop() {};
 
   function rowFull(row) {
     /* Check if a row is full */
@@ -224,226 +225,231 @@ var Tetvas = (function() {
     return Block;
   }
 
-  function Piece(shape, ghost) {
+  function Piece(shape) {
     /*
      * Make a new piece of the specified shape.
      * Shapes : I, O, T, J, L, S, Z
      */
 
-    // Initial points (relative to piece origin) for each shape
-    var SHAPE_POINTS = {
-      'I' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : -1, y : 0 }, { x : 2, y : 0 } ],
-      'O' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : -1 } ],
-      'T' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : 0 } ],
-      'J' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : -1, y : -1 }, { x : 1, y : 0 } ],
-      'L' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 1, y : -1 }, { x : 1, y : 0 } ],
-      'S' : [ { x : 0, y : 0 }, { x : -1, y : 0 }, { x : 0, y : -1 }, { x : 1, y : -1 } ],
-      'Z' : [ { x : 0, y : 0 }, { x : 1, y : 0 }, { x : 0, y : -1 }, { x : -1, y : -1 } ]
-    };
-
-    // Return object
-    var piece = {};
-
     // Colour of this piece
-    piece.fill = SHAPE_FILLS[shape];
-
-    // Set proper fill if we are a ghost piece
-    if (ghost) {
-      piece.fill = GHOST_FILL;
-    } else {
-      //this.ghost = new Piece(shape, ghost);
-    }
+    this.fill = SHAPE_FILLS[shape];
 
     // Shape of piece
-    piece.shape = shape;
+    this.shape = shape;
 
     // Starting point for the piece
     // Origin of the piece (points are relative to this)
-    piece.origin = { x : 4, y : 0 };
-
-    piece.getCoords = function(pt) {
-      /* Get the grid coordinates for a point relative to this piece's origin */
-      return { x : this.origin.x + pt.x, y : this.origin.y + pt.y };
-    };
+    this.origin = { x : 4, y : 0 };
 
     // Points for the piece
-    piece.points = [];
+    this.points = [];
+
+    // Initialize the ghost for this piece
+    this._initGhost();
 
     // Block objects that make up this piece
-    piece.blocks = [];
+    this.blocks = [];
+    this._initBlocks();
+
+  }
+
+  Piece.prototype._initGhost = function() {
+    /* Initialize the ghost of the piece */
+    this.ghost = new Ghost(this.shape);
+  };
+
+  Piece.prototype._initBlocks = function() {
+    /* Initialize the blocks */
 
     // Copy initial points
-    var pts = SHAPE_POINTS[shape];
+    var pts = SHAPE_POINTS[this.shape];
     for (var i = 0; i < pts.length; ++i) {
       var newPoint = copyPoint(pts[i]);
-      piece.points.push(newPoint);
-      piece.blocks.push(new Block(piece.getCoords(newPoint), piece.fill, ghost));
+      this.points.push(newPoint);
+      this.blocks.push(new Block(this.getCoords(newPoint), this.fill));
+    }
+  };
+
+  Piece.prototype.getCoords = function(pt) {
+    /* Get the grid coordinates for a point relative to this piece's origin */
+    return { x : this.origin.x + pt.x, y : this.origin.y + pt.y };
+  };
+
+  Piece.prototype.draw = function() {
+    /* Draw the piece */
+    for (var i = 0; i < this.blocks.length; ++i) {
+      this.blocks[i].draw();
+    }
+  };
+
+  Piece.prototype.undraw = function() {
+    /* Undraw the piece */
+    for (var i = 0; i < this.blocks.length; ++i) {
+      this.blocks[i].undraw();
+    }
+  };
+
+  Piece.prototype.updateBlocks = function() {
+    /*
+     * Update the blocks to have new positions based on
+     * the origin of the piece
+     */
+    for (var i = 0; i < this.blocks.length; ++i) {
+      this.blocks[i].setPoint(this.getCoords(this.points[i]));
+    }
+  };
+
+
+  Piece.prototype.intersects = function(frozenBlocks) {
+    /*
+     * Determine if this piece intersects any frozen blocks
+     */
+
+    for (var i = 0; i < this.blocks.length; ++i) {
+      var coords = this.blocks[i].gridPoint;
+      if (frozenBlocks[coords.y][coords.x]) {
+        return true;
+      }
     }
 
-    piece.draw = function() {
-      /* Draw the piece */
-      for (var i = 0; i < this.blocks.length; ++i) {
-        this.blocks[i].draw();
-      }
-    };
+    return false;
+  };
 
-    piece.undraw = function() {
-      /* Undraw the piece */
-      for (var i = 0; i < this.blocks.length; ++i) {
-        this.blocks[i].undraw();
-      }
-    };
+  Piece.prototype._move = function(frozenBlocks, axis, mag) {
+    /*
+     * Move the piece along the specified axis mag spaces
+     * Returns true if the move is successful (no intersection with frozenBlocks)
+     * Returns false if the move is unsuccessful
+     */
 
-    piece.updateBlocks = function() {
-      /*
-       * Update the blocks to have new positions based on
-       * the origin of the piece
-       */
-      for (var i = 0; i < this.blocks.length; ++i) {
+    // Move along the axis
+    this.origin[axis] += mag;
+
+    // Undraw the blocks, since we might move (but we might not)
+    this.undraw();
+    // Update the blocks to their new positions
+    this.updateBlocks();
+
+    // Check for intersection
+    if (this.intersects(frozenBlocks)) {
+      // It does intersect. Undo the move
+      this.origin[axis] -= mag;
+      this.updateBlocks();
+      this.draw();
+      return false;
+    }
+
+    // No intersection
+    this.draw();
+    return true;
+  };
+
+  /* Functions to move various directions */
+  Piece.prototype.moveLeft = function(frozenBlocks) { return this._move(frozenBlocks, 'x', -1); };
+  Piece.prototype.moveRight = function(frozenBlocks) { return this._move(frozenBlocks, 'x', 1); };
+
+  Piece.prototype.moveDown = function(frozenBlocks) {
+    /* If the move failed then the piece freezes. */
+    return this._move(frozenBlocks, 'y', 1);
+  };
+
+  Piece.prototype.freeze = function(frozenBlocks) {
+    /* Freeze the piece. Also remove full rows. */
+
+    // Track which rows the piece was in when frozen
+    // Use an object as a set
+    var rows = {};
+
+    // Add all blocks to frozenBlocks
+    for (var i = 0; i < this.blocks.length; ++i) {
+      var coords = this.blocks[i].gridPoint;
+      frozenBlocks[coords.y][coords.x] = this.blocks[i];
+
+      rows[coords.y] = true;
+    }
+
+    // We just want the keys
+    rows = Object.keys(rows);
+
+    // Check to see about removing lines
+    // We have to check each row to see if it's full
+    for (var i = 0; i < rows.length; ++i) {
+      if (rowFull(frozenBlocks[rows[i]])) {
+        clearRow(frozenBlocks, rows[i]);
+      }
+    }
+
+  };
+
+  Piece.prototype._rotate = function(frozenBlocks, dir, recur) {
+    /* Rotate the piece in the specified direction. */
+
+    // This one doesn't need to be rotated
+    if (this.shape === 'O') return true;
+
+    // Only undraw the shape if we are not undoing
+    // a previous rotation
+    if (!recur) this.undraw();
+
+    if (this.shape === 'I') {
+      // For I shape just swap x and y
+      for (var i = 0; i < this.points.length; ++i) {
+        // Swap x and y
+        this.points[i] = { x : this.points[i].y, y : this.points[i].x };
         this.blocks[i].setPoint(this.getCoords(this.points[i]));
       }
-    };
 
-
-    piece.intersects = function(frozenBlocks) {
-      /*
-       * Determine if this piece intersects any frozen blocks
-       */
-
-      for (var i = 0; i < this.blocks.length; ++i) {
-        var coords = this.blocks[i].gridPoint;
-        if (frozenBlocks[coords.y][coords.x]) {
-          return true;
-        }
+    } else {
+      // We are doing a normal rotation
+      var p;
+      for (var i = 0; i < this.points.length; ++i) {
+        p = this.points[i];
+        // Swap and make negative (2x2 rotation matrix for pi/2 rotation)
+        this.points[i] = { x : -p.y * dir, y : p.x * dir };
+        this.blocks[i].setPoint(this.getCoords(this.points[i]));
       }
+    }
 
+    // Check for intersection
+    if (!recur && this.intersects(frozenBlocks)) {
+      // If we intersected then we undo the rotation
+      this._rotate(frozenBlocks, -dir, true);
       return false;
-    };
+    }
 
-    piece._move = function(frozenBlocks, axis, mag) {
-      /*
-       * Move the piece along the specified axis mag spaces
-       * Returns true if the move is successful (no intersection with frozenBlocks)
-       * Returns false if the move is unsuccessful
-       */
+    // No intersection - return success
+    this.draw();
+    return true;
 
-      // Move along the axis
-      this.origin[axis] += mag;
-
-      // Undraw the blocks, since we might move (but we might not)
-      this.undraw();
-      // Update the blocks to their new positions
-      this.updateBlocks();
-
-      // Check for intersection
-      if (this.intersects(frozenBlocks)) {
-        // It does intersect. Undo the move
-        this.origin[axis] -= mag;
-        this.updateBlocks();
-        this.draw();
-        return false;
-      }
-
-      // No intersection
-      this.draw();
-      return true;
-    };
-
-    /* Functions to move various directions */
-    piece.moveLeft = function(frozenBlocks) { return this._move(frozenBlocks, 'x', -1); };
-    piece.moveRight = function(frozenBlocks) { return this._move(frozenBlocks, 'x', 1); };
-
-    piece.moveDown = function(frozenBlocks) {
-      /* If the move failed then the piece freezes. */
-      return this._move(frozenBlocks, 'y', 1);
-    };
-
-    piece.freeze = function(frozenBlocks) {
-      /* Freeze the piece. Also remove full rows. */
-
-      // Track which rows the piece was in when frozen
-      // Use an object as a set
-      var rows = {};
-
-      // Add all blocks to frozenBlocks
-      for (var i = 0; i < this.blocks.length; ++i) {
-        var coords = this.blocks[i].gridPoint;
-        frozenBlocks[coords.y][coords.x] = this.blocks[i];
-
-        rows[coords.y] = true;
-      }
-
-      // We just want the keys
-      rows = Object.keys(rows);
-
-      // Check to see about removing lines
-      // We have to check each row to see if it's full
-      for (var i = 0; i < rows.length; ++i) {
-        if (rowFull(frozenBlocks[rows[i]])) {
-          clearRow(frozenBlocks, rows[i]);
-        }
-      }
-
-    };
-
-    piece._rotate = function(frozenBlocks, dir, recur) {
-      /* Rotate the piece in the specified direction. */
-
-      // This one doesn't need to be rotated
-      if (this.shape === 'O') return true;
-
-      // Only undraw the shape if we are not undoing
-      // a previous rotation
-      if (!recur) this.undraw();
-
-      if (this.shape === 'I') {
-        // For I shape just swap x and y
-        for (var i = 0; i < this.points.length; ++i) {
-          // Swap x and y
-          this.points[i] = { x : this.points[i].y, y : this.points[i].x };
-          this.blocks[i].setPoint(this.getCoords(this.points[i]));
-        }
-
-      } else {
-        // We are doing a normal rotation
-        var p;
-        for (var i = 0; i < this.points.length; ++i) {
-          p = this.points[i];
-          // Swap and make negative (2x2 rotation matrix for pi/2 rotation)
-          this.points[i] = { x : -p.y * dir, y : p.x * dir };
-          this.blocks[i].setPoint(this.getCoords(this.points[i]));
-        }
-      }
-
-      // Check for intersection
-      if (!recur && this.intersects(frozenBlocks)) {
-        // If we intersected then we undo the rotation
-        this._rotate(frozenBlocks, -dir, true);
-        return false;
-      }
-
-      // No intersection - return success
-      this.draw();
-      return true;
-
-    };
+  };
 
     // Rotate right and left
-    piece.rotateRight = function(frozenBlocks) { this._rotate(frozenBlocks, 1); };
-    piece.rotateLeft = function(frozenBlocks) { this._rotate(frozenBlocks, -1); };
-
-    return piece;
-  }
+    Piece.prototype.rotateRight = function(frozenBlocks) { this._rotate(frozenBlocks, 1); };
+    Piece.prototype.rotateLeft = function(frozenBlocks) { this._rotate(frozenBlocks, -1); };
 
   /*******************************************************
    * Ghost piece
    *******************************************************/
 
-  function Ghost() {
+  function Ghost(shape) {
+    // Inherit from Piece class
+    Piece.call(this, shape);
   }
-  Ghost.prototype = new Piece();
+  // We inherit from the Piece class, so we need to copy the prototype
+  Ghost.prototype = Object.create(Piece.prototype);
 
-  x = new Ghost('L', '#666');
+  // We don't want to initialize a ghost here
+  Ghost.prototype._initGhost = nop;
+
+  Ghost.prototype._initBlocks = function() {
+    // Copy initial points
+    var pts = SHAPE_POINTS[this.shape];
+    for (var i = 0; i < pts.length; ++i) {
+      var newPoint = copyPoint(pts[i]);
+      this.points.push(newPoint);
+      this.blocks.push(new Block(this.getCoords(newPoint), this.fill, true));
+    }
+  };
 
   /*******************************************************
    * Main driver area
@@ -479,7 +485,7 @@ var Tetvas = (function() {
     Tetvas.frozenBlocks[20][i] = true;
   }
 
-  Tetvas.getNextpiece = function() {
+  Tetvas.getNextPiece = function() {
     /* Get the next piece to generate */
     if (!this.pieceIndex) { shuffle(this.pieceGen); }
 
@@ -503,7 +509,7 @@ var Tetvas = (function() {
     /* Move the piece down. Return true if successful move. */
     if (!this.piece.moveDown(this.frozenBlocks, this)) {
       this.piece.freeze(this.frozenBlocks);
-      this.piece = new Piece(this.getNextpiece());
+      this.piece = new Piece(this.getNextPiece());
       return false;
     }
     return true;
@@ -576,7 +582,7 @@ var Tetvas = (function() {
     this.registerListeners();
 
     // Create the first piece
-    this.piece = new Piece(this.getNextpiece());
+    this.piece = new Piece(this.getNextPiece());
 
     // Start the ticker
     this.togglePause();
@@ -600,6 +606,6 @@ var Tetvas = (function() {
   };
 
   // Play! ( Testing ... )
-  //Tetvas.start();
+  Tetvas.start();
   return Tetvas;
 })();
